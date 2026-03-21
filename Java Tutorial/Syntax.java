c2eeaaae5d009a52f741f3a2154d2a618c2e0290
shap0011 <shap0011@algonquinlive.com>
2026-03-20 23:54:42 -0400

    Initial commit

---
+++ Java Tutorial/Syntax.java
@@ -0,0 +1,33 @@
+ public class Syntax {
+	public static void main (String[] args) {
+		// print text
+		// with line break
+		System.out.println();
+		System.out.println("Hello World!");
+		System.out.println("Have a good day!");
+		System.out.println("Learning Java is fun!");
+		
+		// no line break
+		System.out.println();
+		System.out.print("Hello World! ");
+		System.out.print("I will print on the same line.");
+		
+		// print numbers
+		System.out.println();
+		System.out.println();
+		System.out.println(3);
+		System.out.println(358);
+		System.out.println(50000);
+		
+		// math calculations
+		System.out.println();
+		System.out.println(3 + 3);
+		System.out.println(2 * 5);
+	} 
+ }
+ /* compile
+ C:\Users\Your Name>javac Main.java
+ run
+ C:\Users\Your Name>java Main
+ */
+ 
