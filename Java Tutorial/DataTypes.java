01e0f98463c2b58ee627d6000206450253ae9252
shap0011 <shap0011@algonquinlive.com>
2026-03-21 00:54:31 -0400

    Complete Data Types tutorial

---
+++ Java Tutorial/DataTypes.java
@@ -0,0 +1,27 @@
+public class DataTypes {
+	public static void main (String[] args) {
+		int myNum  = 5;
+		float myFloatNum = 5.99f;
+		char myLetter = 'D';
+		boolean myBool = true;
+		String myText = "Hello";
+		
+		System.out.println(myNum);
+		System.out.println(myFloatNum);
+		System.out.println(myLetter);
+		System.out.println(myBool);
+		System.out.println(myText);
+	}
+}
+
+/* byte	Stores whole numbers from -128 to 127
+short	Stores whole numbers from -32,768 to 32,767
+int	Stores whole numbers from -2,147,483,648 to 2,147,483,647
+long	Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+float	Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+double	Stores fractional numbers. Sufficient for storing 15 to 16 decimal digits
+boolean	Stores true or false values
+char	Stores a single character/letter or ASCII values */
+
+// Once a variable is declared with a type, it cannot change to another type later in the program
+
--- Java Tutorial/Variables.java
+++ Java Tutorial/Variables.java
@@ -1,5 +1,24 @@
 public class Variables {
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		String name = "John";
+		System.out.println(name);
+
+		int myNum = 15;
+		System.out.println(myNum);
+		
+		int myNum2;
+		myNum2 = 20;
+		System.out.println(myNum2);
+		
+		/* final int myNum3 = 25;
+		myNum3 = 30;
+		
+		PS C:\Users\olga_\Java W3S Learning\Java Tutorial> javac Variables.java
+		Variables.java:14: error: cannot assign a value to final variable myNum3
+						myNum3 = 30;
+						^
+		1 error
+		PS C:\Users\olga_\Java W3S Learning\Java Tutorial>
+		*/	
 	}
 }
